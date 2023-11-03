2008bcb58a182483c2a3c5f983b9601657da5b9a
gigs1890 <gigs1890@hotmail.com>
2023-11-04 09:08:00 +1030

    initial file creation and board creation. saved board tiles to variables in script.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const t00 = document.querySelector('.t00');
+const t01 = document.querySelector('.t01');
+const t02 = document.querySelector('.t02');
+const t10 = document.querySelector('.t10');
+const t11 = document.querySelector('.t11');
+const t12 = document.querySelector('.t12');
+const t20 = document.querySelector('.t20');
+const t21 = document.querySelector('.t21');
+const t22 = document.querySelector('.t22');
+
